6a9acb8224d1f39cfd6799bfd58de5db9be68099
roxiqmulnam <roxiqmm@gmail.com>
2023-04-09 19:17:20 +0700

    readd fake template

--- src/components/layouts/Hero.tsx
+++ src/components/layouts/Hero.tsx
@@ -1,23 +1,49 @@
 import React from "react";
 
-type MotionLinkProps = {
+type ListProps = {
   title: string;
-  desc: string;
+  desc?: string;
   url?: string;
 };
 
-const contactList: MotionLinkProps[] = [
+const projectList: ListProps[] = [
   {
-    title: "GitHub",
+    title: "Portfolio",
     desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo, veniam.",
+  },
+  {
+    title: "Documentation",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo, veniam.",
+  },
+  {
+    title: "Landing Page",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo, veniam.",
+  },
+];
+const articleList: ListProps[] = [
+  {
+    title: "HTML",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo, veniam.",
+  },
+  {
+    title: "CSS",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo, veniam.",
+  },
+  {
+    title: "Javascript",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo, veniam.",
+  },
+];
+
+const contactList: ListProps[] = [
+  {
+    title: "Twitter",
   },
   {
     title: "LinkedIn",
-    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo, veniam.",
   },
   {
-    title: "Twitter",
-    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo, veniam.",
+    title: "Github",
   },
 ];
 
@@ -31,13 +57,18 @@ export default function Hero() {
           </h1>
           <h3>I passionate learn about Web Development,</h3>
           <h3>And focusing on Frontend Dev.</h3>
+          <div className="flex gap-10 mt-5">
+            {contactList.map((list) => (
+              <p>{list.title}</p>
+            ))}
+          </div>
         </section>
 
-        <article className="mt-36">
+        <article className="mt-30">
           <h1>Feature Projects</h1>
           <div className="my-5 grid grid-cols-2 gap-4">
-            {contactList.map((link) => (
-              <div className="border rounded-md p-5">
+            {projectList.map((link) => (
+              <div className="rounded-md border p-5">
                 <h3>{link.title}</h3>
                 <p>{link.desc}</p>
                 <br />
@@ -50,8 +81,8 @@ export default function Hero() {
         <article className="mt-10">
           <h1>Feature Articles</h1>
           <div className="my-5 grid grid-cols-2 gap-4">
-            {contactList.map((link) => (
-              <div className="border rounded-md p-5">
+            {articleList.map((link) => (
+              <div className="rounded-md border p-5">
                 <h3>{link.title}</h3>
                 <p>{link.desc}</p>
                 <br />
