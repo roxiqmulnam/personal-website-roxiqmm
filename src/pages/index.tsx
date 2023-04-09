9e1ffa8e5b23fff7f958a78825bcbb9077825da4
roxiqmulnam <roxiqmulna02@gmail.com>
2023-04-09 20:49:10 +0700

    change title to personal website

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import Layout from "@/components/layouts/Layout";
-
+import Head from "next/head";
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Roxiq Mulna Muhammad - Personal Website</title>
+      </Head>
       <Layout>
-
+        <main></main>
       </Layout>
     </>
   );
