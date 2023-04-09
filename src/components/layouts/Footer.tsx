9df566b8e9360de3a98e4c4868cdb2ca8ffae64a
roxiqmulnam <roxiqmulna02@gmail.com>
2023-04-09 19:26:41 +0700

    add horizontal line top on footer

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="layout-component text-white">
+    <footer className="layout-component text-white border-t-2 border-gray-500">
         {new Date().getFullYear()} | Roxiq Mulna Muhammad
     </footer>
   );
